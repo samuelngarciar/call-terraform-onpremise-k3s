996af1f1bb2ba668441651acddd4cc7f26f3fc45
samuelngarciar <84947793+samuelngarciar@users.noreply.github.com>
2023-11-18 12:20:10 -0400

    set v1.24.16+k3s1 and token 12345

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ module "k3s" {
   source  = "app.terraform.io/testsam1/k3s/onpremise"
   version = "1.0.3"
 
-  k3srelease = "v1.28.3+k3s2"
-  k3stoken = 12345678
+  k3srelease = "v1.24.16+k3s1"
+  k3stoken = 12345
 
 }
